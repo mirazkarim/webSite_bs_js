19940af1f5eefdef82b1c1e5dd133df404bbe2e6
mirazkarim <127856906+mirazkarim@users.noreply.github.com>
2023-10-15 23:28:38 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var mixer = mixitup('.filter-item');
+
+
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow:false,
+  nextArrow:false,
+  dots:true,
+});
